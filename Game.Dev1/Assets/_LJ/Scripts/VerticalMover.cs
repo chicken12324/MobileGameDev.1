d62d17a282c94b949ed87185702f8f082eb134e9
chicken12324 <chicken12324@gmail.com>
2019-02-03 15:20:57 +0100

    03.02.2019//15:20:54,81

---
+++ Game.Dev1/Assets/_LJ/Scripts/VerticalMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+  
+  public float movespeed = 10f;
+  public bool enter = true;
+	        var boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//move Object at keypress
+	if (Input.GetKey(KeyCode.LeftArrow))	
+	transform.Translate(Vector3.left * movespeed * Time.deltaTime);
+	
+		if (Input.GetKey(KeyCode.RightArrow))	
+		transform.Translate(Vector3.right * movespeed * Time.deltaTime);
+	
+			if (Input.GetKey(KeyCode.UpArrow))	
+			transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+    
+				if (Input.GetKey(KeyCode.DownArrow))	
+				transform.Translate(-Vector3.forward * movespeed * Time.deltaTime);
+	
+	}
+	 	 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("entered");
+        }
+    }
+}
